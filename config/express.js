7f85e210085d3a262249384cb3df6d541e4bee7d
phichai <phichaipaohom@hotmail.com>
2017-10-07 16:23:38 +0700

    add 8 9

--- config/express.js
+++ config/express.js
@@ -38,5 +38,6 @@ module.exports = function () {
 
     require('../app/routes/index.route')(app);
     require('../app/routes/user.route')(app);
+    require('../app/routes/post.route')(app);
     return app;
 }
